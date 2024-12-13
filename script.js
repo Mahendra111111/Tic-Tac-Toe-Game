235fe217a5025a0ed65a0b297c1352ee6cdd5424
Mahendra111111 <mahendranagpure909@gmail.com>
2024-12-13 19:23:36 +0530

    pushing all the code

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("welcome to Tictactoe game")
+let music = new Audio("music.mp3")
+let turn = new Audio("tine.mp3")
+let gameover = new Audio("gameover.mp3")
+let Turn = "x"
+
+//function to change the turn
+const changeTurn = ()=>{
+    return Turn ==="X"?"0": "X"
+}
+
+//function to check the win
+const checkwin = ()=>{
+    
+}
+
+// game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes)array.forEach(element => {
+    let boxtext = document.querySelector(".boxtext");
+    boxtext.addEventListener('click',()=>{
+        
+        })
+
+});
